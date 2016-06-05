c31b8eb68a1c43b2d054648266843f8265854ebf
Claire Stevenson <c.e.stevenson@uva.nl>
2016-06-05 14:50:10 +0200

    added ratinginstr file and the current database

---
+++ aut_ratinginstr.php
@@ -0,0 +1,47 @@
+<?php 
+include_once('accesscontrol.php');
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="crea.css" />
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+</head>
+<body>
+	<h1 id="title">
+		Beoordelen van Alternatieve Toepassingen
+	</h1>
+	<p>
+		Je bent nu bij het laatste onderdeel aangekomen. In dit
+		onderdeel zal je zowel je eigen als andermans antwoorden
+		beoordelen op basis van drie criteria; originaliteit,
+		bruikbaarheid en creativiteit. 
+		</br></br>
+		Het is van belang dat je
+		gefocust blijft bij dit onderdeel, omdat we anders het
+		onderzoek niet goed kunnen uitvoeren. Neem eventjes pauze als je afgeleid raakt of
+		even geen zin meer hebt. Om te zorgen dat je alles zo goed mogelijk kan beoordelen
+		krijg je nu eerst wat informatie. Lees dit goed door.	 
+		</br></br>
+		Met <b>originaliteit</b> willen we kijken hoe nieuw en onverwacht je
+		een idee vindt. Deze score loopt van 1 tot 10. Hier betekent 1
+		helemaal niet origineel en 10 heel origineel.
+		</br>
+		Met <b>bruikbaarheid</b> willen we kijken hoe geschikt een bepaald
+		idee is om het uit te voeren of te gebruiken. Ook hierbij
+		geldt 1 is absoluut niet bruikbaar en 10 is heel erg
+		bruikbaar.
+		</br>
+		Gebruik voor <b>creativiteit</b> je eigen definitie. Hierbij geldt
+		dat 1 staat voor helemaal niet creatief en 10 voor heel erg
+		creatief.
+	</p>
+	<p>
+		Gebruik de cijfertoetsen om een beoording in te voeren.
+		Gebruik de TAB toets na een beoordeling om snel naar de volgende te gaan.
+	</p>
+	<form method="post" action="expBP2016.php">		
+		<input type="submit" id="submit" name="submit" value="OK">
+	</form>	
+</body>
+</html>
